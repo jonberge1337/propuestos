4ef385333500535b6ab3010d866fbbed1b6f6fd3
jonberge1337 <jonbergerandi@gmail.com>
2019-03-26 14:01:12 +0100

    empezando con consumo

---
+++ src/consumo/Consumo.java
@@ -0,0 +1,15 @@
+package consumo;
+
+public class Consumo {
+	private float kms;
+	private float litros;
+	private int vmed;
+	private float pgas;
+	
+	public float getTiempo() {
+		
+		return kms/vmed;
+	}
+	
+	
+}
