2a32767947cd7f4d1cac2e2cae890b2487cc9b51
jonberge1337 <jonbergerandi@gmail.com>
2019-03-28 11:51:58 +0100

    nomenclatura de edurne

--- src/pajaro27/GestionaPajaro.java
+++ src/pajaro27/GestionaPajaro.java
@@ -2,13 +2,26 @@ package pajaro27;
 
 public class GestionaPajaro {
 
+	public static int nuevopajaro(int n) {
+		n++;
+		
+		return n;
+	}
+	
+	public static void muestrapajaros(int n) {
+		System.out.println(n);
+	}
+	
 	public static void main(String[] args) {
 		Pajaro p1,p2;
-
+		int numpajaros = 0;
+		
 		p1=new Pajaro();
-		Pajaro.muestrapajaros();
+		numpajaros = nuevopajaro(numpajaros);
+		muestrapajaros(numpajaros);
 		p2=new Pajaro('a',3);
-		Pajaro.muestrapajaros();
+		numpajaros = nuevopajaro(numpajaros);
+		muestrapajaros(numpajaros);
 	}
 
 }
